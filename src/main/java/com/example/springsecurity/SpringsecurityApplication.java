207328cca1f835f4ea0511bf18f2a74a4755d833
Rahul Suthar <rahulsuthar@RAHULs-MacBook.local>
2022-05-01 18:12:14 +0530

    build the project

---
+++ src/main/java/com/example/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,16 @@
+package com.example.springsecurity;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class SpringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityApplication.class, args);
+    }
+
+}
